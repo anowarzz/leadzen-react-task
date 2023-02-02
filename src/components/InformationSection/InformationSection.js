1dbc2966883091d8655d61274b632cd8f3d59245
anowarzz <anowarhosen444@gmail.com>
2023-02-02 18:13:49 +0600

    information card designed modified for small device

--- src/components/InformationSection/InformationSection.js
+++ src/components/InformationSection/InformationSection.js
@@ -28,20 +28,20 @@ setClients(response.data)
             {
                 clients && clients?.map(client => <div className= 'flex gap-4 flex-col md:flex-row  bg-white p-4 w-11/12 rounded-lg mx-auto mb-4 items-center' key={client.id}>
                     
-                    <div className='w-full mx-auto'>
-                        <h2 className="font-bold mb-2 text-lg">Company</h2>
-                        <p> {client?.company?.name} </p>
+                    <div className='w-full text-center'>
+                        {/* <h2 className="font-bold md:mb-2 text-lg">Company</h2> */}
+                        <p className='font-semibold'> {client?.company?.name} </p>
                     </div>
-                    <div className='w-full'>
-                <h2 className="font-bold mb-2 text-lg">Contact</h2>
+                    <div className='w-full text-center'>
+                <h2 className="font-semibold md:mb-2 text-lg">Contact</h2>
                 <p>{client?.name}</p>
                     </div>
-                    <div className='w-full'>
-                <h2 className="font-bold mb-2 text-lg">Street</h2>
+                    <div className='w-full text-center'>
+                <h2 className="font-semibold  md:mb-2 text-lg">Street</h2>
                 <p>{client?.address?.street}</p>
                     </div>
-                    <div className='w-full items-center'>
-                <h2 className="font-bold mb-2 text-lg">City</h2>
+                    <div className='w-full text-center'>
+                <h2 className="font-semibold md:mb-2 text-lg">City</h2>
                 <p>{client?.address?.city}</p>
                     </div>
                     <div className=''>
