6319450cf23750b4bf69a758307dd913ed6e2e1f
anowarzz <anowarhosen444@gmail.com>
2023-02-02 14:25:55 +0600

    project initialized with bootstrap css

---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h3>This is the home page</h3>
+        </div>
+    );
+};
+
+export default HomePage;
