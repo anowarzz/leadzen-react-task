4095324cfc6a7270a2c8acdb4907bed10b1c0cec
anowarzz <anowarhosen444@gmail.com>
2023-02-02 15:29:46 +0600

    loaded client data from database using axios and creating table to show data

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import InformationSection from '../../components/InformationSection/InformationSection';
 
 const HomePage = () => {
     return (
         <div className='bg-gray-200 min-h-screen'>
-            <h3 className='text-center text-2xl md:text-3xl lg:text-4xl py-8 '>Client Information of Leadzen</h3>
+            <h3 className='text-center text-2xl md:text-3xl lg:text-4xl py-8 font-bold '>Client Information of Leadzen</h3>
+
+            <InformationSection />
         </div>
     );
 };
--- src/components/InformationSection/InformationSection.js
+++ src/components/InformationSection/InformationSection.js
@@ -1,14 +1,54 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useDebugValue, useEffect, useState } from 'react';
 
 const InformationSection = () => {
 
 
+const [clients, setClients] = useState(null)
 
 
+// Loading data from database through api
+useEffect( () => {
+axios.get("https://jsonplaceholder.typicode.com/users")
+.then(response => {
+console.log(response);
+setClients(response.data)
+})
+.catch((err) => {
+    console.log(err);
     
+})
+
+}, [])
+
+
+
     return (
         <div>
-            
+            {
+                clients && clients?.map(client => <div className= 'flex justify-between bg-white h-24 px-4 w-11/12 rounded-lg mx-auto mb-4 items-center'>
+                    
+                    <div>
+                        <h2>Name</h2>
+                        <p>Imran Dhala</p>
+                    </div>
+                    <div>
+                <h2>Contact</h2>
+                <p>Delhi</p>
+                    </div>
+                    <div>
+                <h2>City</h2>
+                    </div>
+                    <div>
+                <h2>State</h2>
+                    </div>
+                    <div>
+                    <button className='btn btn bg-red-400'>Details</button>
+                    </div>
+                    
+                     </div>
+                     )
+            }
         </div>
     );
 };
