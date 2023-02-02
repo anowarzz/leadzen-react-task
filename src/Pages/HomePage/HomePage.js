3e9de3d2214c1fa9397b86a8a9ff8e1320dcc02a
anowarzz <anowarhosen444@gmail.com>
2023-02-02 15:04:12 +0600

    tailwind css added and primary folder structure created for the project

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const HomePage = () => {
     return (
-        <div>
-            <h3>This is the home page</h3>
+        <div className='bg-gray-200 min-h-screen'>
+            <h3 className='text-center text-2xl md:text-3xl lg:text-4xl py-8 '>Client Information of Leadzen</h3>
         </div>
     );
 };
---
+++ src/components/InformationSection/InformationSection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const InformationSection = () => {
+
+
+
+
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default InformationSection;
